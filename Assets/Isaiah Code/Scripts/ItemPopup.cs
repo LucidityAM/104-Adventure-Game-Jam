a04466755a905c3a1b4f3f839fb6d748036d3bd2
LucidityAM <LucidityAM@gmail.com>
2020-09-17 16:19:18 -0700

    Isaiah

---
+++ Assets/Isaiah Code/Scripts/ItemPopup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class ItemPopup : MonoBehaviour
+{
+    public GameObject popup;
+
+    public Canvas canvas;
+
+    public PlayerController playerController;
+
+    public Object spawnedImage;
+
+    public Animator anim;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spawnedImage = Instantiate(popup, canvas.transform);
+            playerController.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(spawnedImage);
+            playerController.enabled = true;
+        }
+    }
+
+}
