a1b8f76cf745c1850c51eefb5ace2e018c3f2189
LucidityAM <LucidityAM@gmail.com>
2020-09-17 17:28:56 -0700

    Isaiah

---
+++ Assets/Isaiah Code/Scene Transitions/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator anim;
+
+    public int levelToLoad;
+
+
+    public void LoadLevel2or3()
+    {
+
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        anim.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
