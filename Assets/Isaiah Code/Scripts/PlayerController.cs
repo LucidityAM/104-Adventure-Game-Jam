2955cc506e857e78d7563fea95e2c1280025db21
LucidityAM <LucidityAM@gmail.com>
2020-09-19 18:54:53 -0700

    Table movement - Isaiah

--- Assets/Isaiah Code/Scripts/PlayerController.cs
+++ Assets/Isaiah Code/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
 
     public Animator anim;
 
+    private float vertPosition;
+    private float horPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,22 +22,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime); //Moves the player towards the move point always
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f) //If the player is close on the move point, allows them to move again
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3((Input.GetAxisRaw("Horizontal") * .48f), 0f, 0f), .2f, whatStopsMovement))
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3((Input.GetAxisRaw("Horizontal") * .48f), 0f, 0f), .2f, whatStopsMovement)) //Checks if there is something to collide with in the player move spot
                 {
                     movePoint.position += new Vector3((Input.GetAxisRaw("Horizontal") *.48f), 0f, 0f);
-                }
+                    horPosition = Mathf.Abs(Input.GetAxisRaw("Horizontal"));
+                    vertPosition = 0;
+                }//Basic movement, moves player on 48px grid (subject ot change)
+
             } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, (Input.GetAxisRaw("Vertical") * .48f), 0f), .2f, whatStopsMovement))
-                {
+                {    
                     movePoint.position += new Vector3(0f, (Input.GetAxisRaw("Vertical") * .48f), 0f);
-                }
+                    vertPosition = Mathf.Abs(Input.GetAxisRaw("Vertical"));
+                    horPosition = 0;
+                }//Basic movement, moves player on 48px grid (subject ot change)
             }
 
             anim.SetBool("moving", false);
@@ -55,22 +63,24 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("up");
                 collision.transform.position += new Vector3(0f, .48f, 0f);
             }
-            else if (relativePosition.x < 0)
+            else if (vertPosition == 0)
             {
-                Debug.Log("left");
-                collision.transform.position += new Vector3(-.48f, 0f, 0f);
-            }
-            else if (relativePosition.x > 0)
-            {
-                Debug.Log("right");
-                collision.transform.position += new Vector3(.48f, 0f, 0f);
+                if (relativePosition.x < 0)
+                {
+                    Debug.Log("left");
+                    collision.transform.position += new Vector3(-.48f, 0f, 0f);
+                }
+                else if (relativePosition.x > 0)
+                {
+                    Debug.Log("right");
+                    collision.transform.position += new Vector3(.48f, 0f, 0f);
+                }
             }
             else if (relativePosition.y < 0)
             {
                 Debug.Log("down");
                 collision.transform.position += new Vector3(0f, -.48f, 0f);
             }
-           
-        }
+        } //Checks where the player is in relationship to the moveable object and then moves the object accordingly
     }
 }
