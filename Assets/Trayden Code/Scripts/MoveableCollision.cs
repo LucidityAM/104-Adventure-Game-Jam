4f9cb42185980658852554a8a74a17ee16273337
LucidityAM <LucidityAM@gmail.com>
2020-09-26 13:17:08 -0700

    movement - Isaiah

--- Assets/Trayden Code/Scripts/MoveableCollision.cs
+++ Assets/Trayden Code/Scripts/MoveableCollision.cs
@@ -4,32 +4,39 @@ using UnityEngine;
 
 public class MoveableCollision : MonoBehaviour
 {
+    public GameObject Object;
     public GameObject Player;
+
+    public void Start()
+    {
+        Object = this.gameObject;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if (collision.gameObject.CompareTag("Moveable"))
+        if (collision.gameObject.CompareTag("Moveable") || collision.gameObject.CompareTag("Stationary"))
         {
             var relativePosition = transform.InverseTransformPoint(collision.transform.position);
 
             if (relativePosition.y > 0)
             {
-                Player.GetComponent<PlayerController>().movePoint.transform.position += new Vector3(0f, .72f, 0f);
+                Object.transform.position += new Vector3(0f, .72f, 0f);
             }
             else if (Player.GetComponent<PlayerController>().vertPosition == 0)
             {
                 if (relativePosition.x < 0)
                 {
-                    Player.GetComponent<PlayerController>().movePoint.transform.position += new Vector3(-.72f, 0f, 0f);
+                    Object.transform.position += new Vector3(-.72f, 0f, 0f);
                 }
                 else if (relativePosition.x > 0)
                 {
-                    Player.GetComponent<PlayerController>().movePoint.transform.position += new Vector3(.72f, 0f, 0f);
+                    Object.transform.position += new Vector3(.72f, 0f, 0f);
                 }
             }
             else if (relativePosition.y < 0)
             {
-                Player.GetComponent<PlayerController>().movePoint.transform.position += new Vector3(0f, -.72f, 0f);
+               Object.transform.position += new Vector3(0f, -.72f, 0f);
             }
 
             if (collision.gameObject.CompareTag("Wall"))
