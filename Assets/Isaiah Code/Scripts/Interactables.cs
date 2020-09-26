b811f1b7f74f780e8d2078c879cc9d185a8dc627
LucidityAM <LucidityAM@gmail.com>
2020-09-26 15:24:09 -0700

    Fades - Isaiah

---
+++ Assets/Isaiah Code/Scripts/Interactables.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var relativePosition = transform.InverseTransformPoint(collision.transform.position);
+
+            if (relativePosition.y > 0)
+            {
+                collision.transform.position += new Vector3(0f, .72f, 0f);
+            }
+            else if (collision.gameObject.GetComponent<PlayerController>().vertPosition == 0)
+            {
+                if (relativePosition.x < 0)
+                {
+                    collision.transform.position += new Vector3(-.72f, 0f, 0f);
+                }
+                else if (relativePosition.x > 0)
+                {
+                    collision.transform.position += new Vector3(.72f, 0f, 0f);
+                }
+            }
+            else if (relativePosition.y < 0)
+            {
+                collision.transform.position += new Vector3(0f, -1.44f, 0f);
+            }
+        }
+    }
+}
--- Assets/Isaiah Code/Scripts/PlayerController.cs
+++ Assets/Isaiah Code/Scripts/PlayerController.cs
@@ -81,6 +81,38 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        if (collision.gameObject.CompareTag("Interactable"))
+        {
+            var relativePosition = transform.InverseTransformPoint(collision.transform.position);
+
+            if (relativePosition.y > 0)
+            {
+               movePoint.transform.position += new Vector3(0f, -.72f, 0f);
+            }
+            else if (vertPosition == 0)
+            {
+                if (relativePosition.x < 0)
+                {
+
+                    movePoint.transform.position += new Vector3(.72f, 0f, 0f);
+
+
+                }
+                else if (relativePosition.x > 0)
+                {
+
+                    movePoint.transform.position += new Vector3(-.72f, 0f, 0f);
+                }
+            }
+            else if (relativePosition.y < 0)
+            {
+
+                movePoint.transform.position += new Vector3(0f, .72f, 0f);
+
+            }
+        }
+
         if(collision.gameObject.CompareTag("Moveable"))
         {
             var relativePosition = transform.InverseTransformPoint(collision.transform.position);
