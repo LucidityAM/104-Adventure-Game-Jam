3bbb5574d98b983e133a9b1cd493eaeaf19c6abf
LucidityAM <65725539+LucidityAM@users.noreply.github.com>
2020-09-27 11:34:51 -0700

    Tris: adding goals finishing shit idk

---
+++ Assets/Tris Code/Credits/Scripts/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("EndScene");
+    }
+
+
+    public IEnumerator EndScene()
+    {
+        yield return new WaitForSeconds(43f);
+        SceneManager.LoadScene("Main Menu");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
